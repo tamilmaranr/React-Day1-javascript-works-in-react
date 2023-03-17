2ef166f8b77127071a5102f4ee4a4ea65a4b395d
tamilmaranr <108916935+tamilmaranr@users.noreply.github.com>
2023-03-17 10:17:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Message to Devoloper")
+function Alertmessage(){
+    const Hi= ()=>{alert("Message from javascript alert!")}
+return<div><button onClick={Hi}>Click</button></div>
+}
+ReactDOM.render(<Alertmessage/>,document.getElementById('mydiv'))
